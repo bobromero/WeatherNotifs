f9aebbee8561617c2c81541235d153c2bf322a09
bobromero <rromero123102@gmail.com>
2025-01-21 12:48:51 -0500

    changed website for getting information

--- main.py
+++ main.py
@@ -7,20 +7,16 @@ def getdata(url):
     return r.text
 
 
-htmldata = getdata("https://weather.com/weather/tenday/l/Tallahassee+FL?canonicalCityId=f7dbdd0b7c2e54f47aa09058fc60f9fce2af5b7d76ffe3487e30d6edd47684bd")
+htmldata = getdata("https://weather.com/weather/today/l/Tallahassee+FL?canonicalCityId=f7dbdd0b7c2e54f47aa09058fc60f9fce2af5b7d76ffe3487e30d6edd47684bd")
 
 soup = BeautifulSoup(htmldata, 'html.parser')
 
-#print(soup.prettify())
 
 current_temp = soup.find_all("span",  
-                             class_="DailyContent--temp--axgOn DailyContent--tempN--DPsDJ") 
-chances_rain = soup.find_all("span",  
-                             class_= "DetailsTable--value--pWEVz") 
+                             class_="CurrentConditions--tempValue--zUBSz") 
 temp = (str(current_temp))
-temp_rain = str(chances_rain)
 
-result = "current_temp " + temp[109:111] + " in Tallahassee " + "\n" + "Humidity of " + temp_rain[72:75]
+result = "\nCurrent Temp " + temp[92:94]+ " in Tallahassee"
 print("Weather update", result)
 
 
